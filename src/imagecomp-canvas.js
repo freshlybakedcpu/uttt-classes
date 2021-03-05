d8b83c0eebc3caab7837cfa662e190f2b0429626
freshlybakedcpu <btl2202@iolani.org>
2021-03-04 14:59:18 -1000

    Worked on node-canvas code

--- src/imagecomp-canvas.js
+++ src/imagecomp-canvas.js
@@ -1,9 +1,22 @@
 const nodeCanvas = require('canvas');
+const fs = require('fs');
+const { spacePositions, squares, innerSquares } = require('../json/uttt.json');
 
 const canvas = nodeCanvas.createCanvas(600, 600);
 const ctx = canvas.getContext('2d');
 
-nodeCanvas.loadImage('images/board.png').then(image => {
-    ctx.drawImage(image, 50, 0, 70, 70);
-    // console.log('<img src="' + canvas.toDataURL() + '" />');
-  })
+function repeat() {
+	nodeCanvas.loadImage('images/board.png').then(board => {
+		ctx.drawImage(board, 0, 0);
+		nodeCanvas.loadImage('images/tictactoe_O.png').then(piece_O => {
+			const randOne = Math.floor(Math.random() * squares.length);
+			const randTwo = Math.floor(Math.random() * innerSquares.length);
+
+			ctx.drawImage(piece_O, spacePositions[squares[randOne] + innerSquares[randTwo]][0], spacePositions[squares[randOne] + innerSquares[randTwo]][1], 60, 60);
+			const buffer = canvas.toBuffer('image/png');
+			fs.writeFileSync('./images/image.png', buffer);
+		});
+	});
+}
+
+setInterval(repeat, 2000);
