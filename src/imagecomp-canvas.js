2f9ac8740ef7c8e983c147b6bad363d72a10136e
freshlybakedcpu <btl2202@iolani.org>
2021-03-04 14:42:07 -1000

    Installed eslint

--- src/imagecomp-canvas.js
+++ src/imagecomp-canvas.js
@@ -1,1 +1,9 @@
-const sharp = require('canvas');
+const nodeCanvas = require('canvas');
+
+const canvas = nodeCanvas.createCanvas(600, 600);
+const ctx = canvas.getContext('2d');
+
+nodeCanvas.loadImage('images/board.png').then(image => {
+    ctx.drawImage(image, 50, 0, 70, 70);
+    // console.log('<img src="' + canvas.toDataURL() + '" />');
+  })
