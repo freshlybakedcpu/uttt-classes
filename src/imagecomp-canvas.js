711fcf183cb35516aeef2f764441489eee591805
freshlybakedcpu <btl2202@iolani.org>
2021-03-09 11:42:52 -1000

    Fixed piece image plotting

--- src/imagecomp-canvas.js
+++ src/imagecomp-canvas.js
@@ -1,5 +1,6 @@
 const nodeCanvas = require('canvas');
 const fs = require('fs');
+const { resolve } = require('path');
 const { spacePositions, squares, innerSquares } = require('../json/uttt.json');
 
 const canvas = nodeCanvas.createCanvas(600, 600);
@@ -7,7 +8,6 @@ const ctx = canvas.getContext('2d');
 
 const boardstate = [];
 let player = 'X';
-let buffer = canvas.toBuffer('image/png');
 
 const numTimes = Math.floor(Math.random() * (10 - 1) + 1); // The maximum is exclusive and the minimum is inclusive
 for(let i = 0; i < numTimes; i++) {
@@ -18,8 +18,11 @@ for(let i = 0; i < numTimes; i++) {
 // https://stackoverflow.com/questions/18983138/callback-after-all-asynchronous-foreach-callbacks-are-completed
 nodeCanvas.loadImage('images/board.png').then(board => {
 	ctx.drawImage(board, 0, 0);
-	buffer = canvas.toBuffer('image/png');
 
+	boardstate.forEach(e => {
+		drawPiece(e);
+	});
+	/*
 	const requests = boardstate.map((e) => {
 		return new Promise((resolve) => {
 			if(e.charAt(0) === 'X') {
@@ -34,31 +37,19 @@ nodeCanvas.loadImage('images/board.png').then(board => {
 
 	Promise.all(requests).then(() => {
 		console.log('done');
-		fs.writeFileSync('./images/image.png', buffer);
+		fs.writeFileSync('./images/image.png', canvas.toBuffer('image/png'));
 	});
-	/*
-	boardstate.forEach(e => {
-		if(e.charAt(0) === 'X') {
-			drawX(e.substring(1));
-		}
-		else {
-			drawO(e.substring(1));
-		}
-	});
-	fs.writeFileSync('./images/image.png', buffer);
 	*/
+
+	console.log('done');
+	// fs.writeFileSync('./images/image.png', canvas.toBuffer('image/png'));
 });
 
-async function drawO(piece) {
-	nodeCanvas.loadImage('images/tictactoe_O.png').then(piece_O => {
-		ctx.drawImage(piece_O, spacePositions[piece][0], spacePositions[piece][1], 60, 60);
-		buffer = canvas.toBuffer('image/png');
-	});
-}
-
-async function drawX(piece) {
-	nodeCanvas.loadImage('images/tictactoe_X.png').then(piece_X => {
-		ctx.drawImage(piece_X, spacePositions[piece][0], spacePositions[piece][1], 60, 60);
-		buffer = canvas.toBuffer('image/png');
+async function drawPiece(piece) {
+	nodeCanvas.loadImage(`images/tictactoe_${piece.charAt(0)}.png`).then(pieceImage => {
+		ctx.drawImage(pieceImage, spacePositions[piece.substring(1)][0], spacePositions[piece.substring(1)][1], 60, 60);
+		fs.writeFileSync('./images/image.png', canvas.toBuffer('image/png'));
+	}).catch(err => {
+		console.log(err);
 	});
 }
